a8976ebe0c23ec902e1c22974be4b8f954c50f6a
sanobar28 <sanobar.mujawar@gmail.com>
2021-07-12 15:06:18 +0530

    added: calculate total emp wage

--- employeeWage.js
+++ employeeWage.js
@@ -8,6 +8,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 
 let empHrs = 0;
 empCheck = Math.floor(Math.random() * 10) % 3;
@@ -28,12 +29,13 @@ function getWorkingHours(empCheck) {
 }
 
 /**
- * calculating wage for no. of working days 
+ * calculating total emp wage based on working hrs
  */
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-       empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("emp Wage: " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days: " + totalWorkingDays + "Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
